f48023b3cbc41e45d1bdbc65ab8768f8f1bcc6b1
zalceee <zalcee.songodanan@trcon.ph>
2026-08-17 09:08:56 +0800

    Update createTask.bat

--- createTask.bat
+++ createTask.bat
@@ -1,3 +1,12 @@
 @echo off
+set "APP=%~dp0cgt.exe"
 
-schtasks /create /tn "CCTVGridTool" /tr "%cd%\cgt.exe" /sc onlogon /rl highest /f
+schtasks /create /tn "CCTVGridTool" /tr "\"%APP%\"" /sc onlogon /rl highest /f
+
+if %errorlevel% equ 0 (
+    echo CCTVGridTool task created successfully.
+) else (
+    echo Failed to create CCTVGridTool task.
+)
+
+pause
